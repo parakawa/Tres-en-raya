42ca3ca69191c2542ef1a66523257eb1b3ca294d
parakawa <paty.arakawa@gmail.com>
2016-05-12 17:56:39 -0500

    no hay empate, aún

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 
 $(document).ready(function(){
-	$(".casilla").on("click",function(e){
+	$(".casilla").on("click",marcar);
 		$(this).html("x")
-	});
-	$(".casilla").on("dblclick",function(e){
-		$(this).html("o")
-	});
-	$(".reset").on("click",function(e){
+
+		$(".reset").on("click",function(e){
 		$(".casilla").html("")
 	});
 
@@ -22,26 +19,38 @@ $(document).ready(function(){
 		var x9=$("#x9").text()
 
 		if(x1==x2 && x2==x3 && x1!="")
-			alert("Jugador de la marca "+x1+ "gana");
+			alert("Jugador de la marca "+x1+ " gana");
 		if(x4==x5 && x5==x6 && x4!="")
-			alert("Jugador de la marca "+x4+ "gana");
+			alert("Jugador de la marca "+x4+ " gana");
 		if(x7==x8 && x8==x9 && x7!="")
-			alert("Jugador de la marca "+x7+ "gana");
+			alert("Jugador de la marca "+x7+ " gana");
 		if(x1==x4 && x4==x7 && x1!="")
-			alert("Jugador de la marca "+x1+ "gana");
+			alert("Jugador de la marca "+x1+ " gana");
 		if(x2==x5 && x5==x8 && x2!="")
-			alert("Jugador de la marca "+x2+ "gana");
+			alert("Jugador de la marca "+x2+ " gana");
 		if(x3==x6 && x6==x9 && x3!="")
-			alert("Jugador de la marca "+x3+ "gana");
+			alert("Jugador de la marca "+x3+ " gana");
 		if(x1==x5 && x5==x9 && x1!="")
-			alert("Jugador de la marca "+x1+ "gana");
+			alert("Jugador de la marca "+x1+ " gana");
 		if(x3==x5 && x5==x7 && x3!="")
-			alert("Jugador de la marca "+x3+ "gana");
+			alert("Jugador de la marca "+x3+ " gana");
 
 		
 	});
-	
-	
+
 });
+
+var i=0;
+function marcar(){
+	i++;
+	if(i%2==0)
+		$(this).text("o");
+	else
+		$(this).text("x");
+}
+	
+	
+	
+
 
 
